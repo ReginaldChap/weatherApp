fabb780906305f674c0de830dd6d67470d453e9d
Jesse <jesse@jessechapman.com.au>
2018-02-04 16:12:43 +1100

    finished section app using promises

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const yargs = require('yargs');
+const axios = require('axios');
 
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
 
 const argv = yargs
     .options({
@@ -16,17 +15,26 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(`Error: ${errorMessage}`)
-    } else {
-        console.log(results.address);
-        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-            if (errorMessage) {
-                console.log(`error: ${errorMessage}`);
-            } else {
-                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
-            }
-        });
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
+
+axios.get(geocodeUrl).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find address');
     }
-});
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/6ac7289b52348a595a1636c1cf1d96de/${lat},${lng}`
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl)
+}).then((response) => {
+    var temp = response.data.currently.temperature;
+    var apparentTemp = response.data.currently.apparentTemperature;
+    console.log(`the temp is ${temp}, but it feels like ${apparentTemp}`);
+}).catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+        console.log("unable to connect to API servers");
+    } else {
+        console.log(e.message);
+    }
+})
